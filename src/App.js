84847b1cb18aa53f7db63d27d1af5e623baa29b6
Elliot Scotto <112620044+ElliotScotto@users.noreply.github.com>
2022-11-01 16:37:04 +0100

    component Section is OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import Section from "./Components/Section";
+import data from "./data.json";
+// import category from "./data.json";
+// import images from "./data.json";
+//
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {data.map((elem, index) => {
+        return <Section />;
+      })}
+      {/* <div className="section">
+        <p>Titre de la Catégorie</p>
+        <div className="carrousel">
+          <div className="moviepic"></div>
+        </div>
+      </div> */}
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Section.js
@@ -0,0 +1,9 @@
+const Section = (props) => {
+  return (
+    <div>
+      <h2>Test Catégorie</h2>
+      <p>"Test image"</p>
+    </div>
+  );
+};
+export default Section;
